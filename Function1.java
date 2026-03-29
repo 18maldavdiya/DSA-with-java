e2eaa6fd685311a82e1f0d6ff379642e84e04de2
18maldavdiya <hanish2005b@gmail.com>
2026-03-29 08:51:04 +0530

    Function

---
+++ Function1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Function1 {
+    public static void hello(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello, how are you?");
+    }
+    public static void main(String args []){
+        hello();
+    }
+}

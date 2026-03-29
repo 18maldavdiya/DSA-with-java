5fec058d0bb5da225b82a2a191dab211a056a322
18maldavdiya <hanish2005b@gmail.com>
2026-03-29 09:57:31 +0530

    Binomial_Coefficent

---
+++ Bionomial_Coefficent.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bionomial_Coefficent {
+     public static int factorial(int n){
+        int f = 1;
+        for(int i= 1;i<=n;i++){
+            f = f*i;
+        }
+        return f;
+
+    }
+    public static int binomial(int n ,int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_n_r = factorial(n-r);
+        int binomial = fact_n/(fact_r*fact_n_r);
+        return binomial;
+        }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int binom = binomial(n,r);
+        System.out.println("bino of" + n + " and " + r + " is " + binom);
+    }
+}
